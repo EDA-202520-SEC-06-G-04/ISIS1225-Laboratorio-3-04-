7e83d500e3641951b10a098556804155052ffc7b
JUANPABLO2007\penar <penarandajuan868@gmail.com>
2025-08-20 15:18:43 -0500

    jgj

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -1,4 +1,19 @@
-def new_list{
+
+def new_list():
     
-}
-pass
+    return {'size': 0, 'elements': []}
+
+def add_first(my_list, element):
+    
+    my_list['elements'].insert(0, element)
+    my_list['size'] += 1
+    return my_list
+
+
+
+
+    
+def add_last():
+    pass
+    
+    
